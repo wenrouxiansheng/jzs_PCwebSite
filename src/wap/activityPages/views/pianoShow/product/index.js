c90730edb3b61cf91dff19f9d1fd422ad365f481
代裔函 <1686518809@qq.com>
2021-08-24 02:32:23 +0000

    修改型号

--- src/wap/activityPages/views/pianoShow/product/index.js
+++ src/wap/activityPages/views/pianoShow/product/index.js
@@ -12,7 +12,7 @@ export default class Product extends Component {
     pianoSlideList: [
       {
         image: require("../../../assets/piano/121B.png").default,
-        title: "JZS-121B6",
+        title: "JZS-121B",
         subtitle: "简约奢华品质美感",
         price: "24800",
         link: "/site/wap/activity/pianoList",

dc223fe318d36d89a0f609536ad513d2ae5bce5a
YH <1686518809@qq.com>
2021-08-25 17:52:36 +0800

    修改钢琴信息

--- src/wap/activityPages/views/pianoShow/productDetail/index.js
+++ src/wap/activityPages/views/pianoShow/productDetail/index.js
@@ -12,16 +12,15 @@ const productDetail = [
   {
     img: require('../../../assets/piano/121B.png').default,
     from: '中国',
-    color: '黑色亮光弯腿',
-    size: '1500mm*620mm*1210mm',
+    color: '黑色亮光',
+    size: '1500mm*1210mm*610mm',
     musicWire: '德国“ROSLAU”勒斯劳琴弦',
-    plank: null,
-    hammer: '德国顶级羊毛毡红木榔头',
+    plank: '高级镀铬踏板',
+    hammer: '红木芯德国进口毛毡',
     key: '白松复合实木白键',
     nail: null,
     number: '121B',
-    trait:
-      '外观简洁大方，平面右盘配衬方形虎脚，内置进口双缓降器,采用家庭用琴配置，是一款性价比较高的入门级钢琴。',
+    trait: null,
     mark: '翻砂蝶形铁骨工艺，一次成型',
     soundboard: '云杉实木复合精制音板',
     oilPaint: '德国无氧无污染天然酵化镜面漆',
@@ -33,15 +32,15 @@ const productDetail = [
     img: require('../../../assets/piano/123B.png').default,
     from: '中国',
     color: '黑色亮光',
-    size: '1530mm*1230mm*610mm​',
+    size: '1530mm*1230mm*610mm',
     musicWire: '德国“ROSLAU”勒斯劳琴弦',
     plank: '高级镀铜踏板',
-    hammer: '德国顶级羊毛毡红木榔头',
+    hammer: '红木芯德国进口毛毡',
     key: '白松复合实木白键并逐键加铅',
     nail: null,
     number: '123B7',
-    trait: '三上门拼接、弯腿。',
-    mark: '全框铁骨工艺，一次成型，保证更大拉力',
+    trait: null,
+    mark: '全框铁骨翻砂工艺，一次成型，保证更大拉力',
     soundboard: '云杉实木复合精制音板',
     oilPaint: '德国无氧无污染天然酵化镜面漆',
     slowDescent: '高端内置缓降器',
@@ -52,18 +51,18 @@ const productDetail = [
     img: require('../../../assets/piano/126B.png').default,
     from: '中国',
     color: '黑色亮光',
-    size: '1530mm*1260mm*610mm​',
+    size: '1530mm*1260mm*610mm',
     musicWire: '德国“ROSLAU”勒斯劳琴弦',
-    plank: '高级镀铜踏板',
-    hammer: '德国顶级羊毛毡红木榔头',
+    plank: '高级镀铜拉丝踏板',
+    hammer: '红木芯德国进口毛毡',
     key: '白松复合实木白键并逐键加铅',
     nail: null,
     number: '126B',
-    trait: '大谱台设计、弯腿',
+    trait: null,
     mark: '全框铁骨工艺，一次成型，保证更大拉力',
     soundboard: '云杉实木复合精制音板',
     oilPaint: '德国无氧无污染天然酵化镜面漆',
-    slowDescent: '高端内置缓降器和三角琴同款',
+    slowDescent: '高端内置缓降器和三角同款',
     action: '雷诺式击弦机',
     MovingRod: '品质天然实木联动装置',
   },
@@ -115,7 +114,12 @@ export default class ProductDetail extends Component {
         <div className="piano-detail-parameter">
           <span>品牌</span>
           <span>JUZISHU</span>
-          <div className="text">外形特点：{data.trait}</div>
+          <div
+            className="text"
+            style={{ display: data.trait ? 'block' : 'none' }}
+          >
+            外形特点：{data.trait}
+          </div>
           <div className="text">配置详情：</div>
           <p>
             产地<span>{data.from}</span>
